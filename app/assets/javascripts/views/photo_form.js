ce7906137064e0b9dd5434dafb883204eba6634a
Minsoo <minsoo91@gmail.com>
2014-11-26 11:44:21 -0800

    Consume google autocomplete api

--- app/assets/javascripts/views/photo_form.js
+++ app/assets/javascripts/views/photo_form.js
@@ -1,9 +1,9 @@
 window.Galbissam.Views.PhotoForm = Backbone.View.extend({
-	// Two Choose Image buttons rendered 
-	// Upload method doesn't work
+	className: "upload-form-wrapper",
 	events: {
 		"submit form": "upload",
-		"change input[type='filepicker']": "preview"
+		"change input[type='filepicker']": "preview",
+		// "keyup #photo_place": "googlePlaceSearch"
 	},
 	template: JST['photos/form'],
 	render: function () {
@@ -16,6 +16,9 @@ window.Galbissam.Views.PhotoForm = Backbone.View.extend({
 	  event.preventDefault();
 	  $form = $(event.currentTarget)
 	  var url = $('input[type="filepicker"]').val()
+	  if (url.length === 0) {
+	  	$('#upload-preview').html("<div class='alert-danger'>Please pick a file first</div>")
+	  }
 	  var review = $form.find('input[type=text]').val()
 	    var newPhoto = new Galbissam.Models.Photo();
 	    newPhoto.save({ "review": review, "filepicker_url": url }, {
@@ -28,5 +31,10 @@ window.Galbissam.Views.PhotoForm = Backbone.View.extend({
 	preview: function () {
 		var url = $('input[type="filepicker"]').val()
 		$('#upload-preview').html("<img src=" + url + ">");
-	}
+		$('#upload-preview').append("<input type='text' id='photo_review' class='form-control' name='photo[review]' style='width: 400px' placeholder='Write something about your food here'>");
+		$('#upload-preview').append("<input type='text' id='photo_place' class='form-control' name='photo[place]' style='width: 400px' placeholder='Where is this?'>")
+		input = document.getElementById('photo_place');
+		var autocomplete = new google.maps.places.Autocomplete(input);
+	},
+
 });
