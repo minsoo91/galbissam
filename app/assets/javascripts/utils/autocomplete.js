a7450b6d81f4b3e5588e719263e5fbe27741cb50
Minsoo <minsoo91@gmail.com>
2014-12-02 14:25:16 -0800

    Implement Bloodhound and TypeAhead library

--- app/assets/javascripts/utils/autocomplete.js
+++ app/assets/javascripts/utils/autocomplete.js
@@ -1,36 +1,40 @@
-var statez = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
-
 $(document).ready(function() {
-	var states = new Bloodhound({
-	  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-	  queryTokenizer: Bloodhound.tokenizers.whitespace,
-	  // `states` is an array of state names defined in "The Basics"
-	  local: $.map(statez, function(state) { return { value: state }; })
-	});
+	var menuItems = []
+
+	$.ajax('api/photos', {
+		success: function (data) {
+			for (var i = 0; i < data.length; i++) {
+				menuItems.push(data[i].name)
+			}
+			var foodItems = new Bloodhound({
+			  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+			  queryTokenizer: Bloodhound.tokenizers.whitespace,
+			  local: $.map(menuItems, function(photo) { return { value: photo }; })
+			});
 	 
 	// kicks off the loading/processing of `local` and `prefetch`
-	states.initialize();
+			foodItems.initialize();
 	 
-	$('#search').typeahead({
-	  hint: true,
-	  highlight: true,
-	  minLength: 1
-	},
-	{
-	  name: 'states',
-	  displayKey: 'value',
-	  // `ttAdapter` wraps the suggestion engine in an adapter that
-	  // is compatible with the typeahead jQuery plugin
-	  source: states.ttAdapter()
+			$('#search').typeahead({
+			  hint: true,
+			  highlight: true,
+			  minLength: 1
+			},
+			{
+			  name: 'photos',
+			  displayKey: 'value',
+			  // `ttAdapter` wraps the suggestion engine in an adapter that
+			  // is compatible with the typeahead jQuery plugin
+			  source: foodItems.ttAdapter(),
+			  templates: {
+			  	header: '<h4 class="dataset-header">Menu Items</h4>'
+			  }
+			}
+			// ,{
+			// 	name: 'Restaurants',
+			// 	displayKey: 'value',}
+			);
+		}
 	});
 });
 
